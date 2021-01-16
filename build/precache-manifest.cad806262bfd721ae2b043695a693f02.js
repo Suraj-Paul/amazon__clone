8e9abb7ba4cbf4608487616bb0bc4a8e68321f72
Suraj-Paul <paulsuraj61@gmail.com>
2021-01-16 18:03:59 +0530

    Modified

---
+++ build/precache-manifest.cad806262bfd721ae2b043695a693f02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c71564487584ca218ed7551bcfeac8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01edbc0c08ee9f11c0e0",
+    "url": "/static/css/2.1a0a2c47.chunk.css"
+  },
+  {
+    "revision": "a96736ffc553c9cd7bc9",
+    "url": "/static/css/main.93df2d58.chunk.css"
+  },
+  {
+    "revision": "01edbc0c08ee9f11c0e0",
+    "url": "/static/js/2.4bf3da67.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.4bf3da67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96736ffc553c9cd7bc9",
+    "url": "/static/js/main.19d4db27.chunk.js"
+  },
+  {
+    "revision": "490c7f883f9f2429e771",
+    "url": "/static/js/runtime-main.3dc22f2b.js"
+  }
+]);
